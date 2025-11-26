81c0ac111665441d97adb21542486a93ca5e4d4c
ronitdhi-glitch <ronitdhi@usc.edu>
2025-11-26 13:18:09 -0800

    Update state_analysis.py

--- src/state_analysis.py
+++ src/state_analysis.py
@@ -1,22 +1,20 @@
 def filter_high_risk_jobs(df):
-    """
-    Returns only occupations where Probability > 50%
-    """
+    # Returns only occupations where Probability is greater than the median 0.640.
     return df[df["probability"] > 0.640]
 
 
 def get_top_states(df):
-    """
-    Sums job loss risk for each state column.
-    """
+    
+    # Returns the top 10 states with the highest estimated job loss risk.
     state_columns = df.columns[3:]   # skip soc, occupation, probability
 
-    # multiply probability * employment count per state
+    # Created a dictionary to store state level job loss estimates. 
     state_loss = {}
     for state in state_columns:
+    # multiply probability * employment count per state and then sum across all occupations. 
         state_loss[state] = (df[state] * df["probability"]).sum()
 
-    # sort and convert to dataframe
+    
     import pandas as pd
     result = pd.DataFrame({
         "state": state_loss.keys(),
@@ -27,6 +25,7 @@ def get_top_states(df):
 
 
 def get_bottom_states(df):
+    # Calculates and return the 10 states with the lowest estimated job loss.
     state_columns = df.columns[3:]
 
     state_loss = {}
