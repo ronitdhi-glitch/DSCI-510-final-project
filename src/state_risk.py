2124ac2487b84529c2989aa75dabe13484b0900b
ronitdhi-glitch <ronitdhi@usc.edu>
2025-11-26 13:21:43 -0800

    Update state_risk.py

--- src/state_risk.py
+++ src/state_risk.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+# This function calculates nimber of high risk automation jobs specifically for the state of california. 
 def calculate_state_risky_jobs(file_path):
     df = pd.read_csv(file_path, encoding="latin1")  
 
@@ -8,10 +8,11 @@ def calculate_state_risky_jobs(file_path):
 
     median_prob = probabilities.median()
     high_risk = df[probabilities >= median_prob]
-
+    # multiply California employment by automation probability to estimate toal number of risky jobs in the state. 
     total_risky_jobs = (high_risk["California"] * high_risk["Probability"]).sum()
 
     risky_csv_path = "data/risky_jobs_california.csv"
     high_risk.to_csv(risky_csv_path, index=False)
 
     return total_risky_jobs, median_prob, risky_csv_path
+
